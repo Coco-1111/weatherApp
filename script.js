7b0daccd0d1fbb5f4a3612f90e5be9ff6c780c07
coco.r <coco.r8607@gmail.com>
2022-12-09 18:00:47 -0600

    recoded .js

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-let weather = {
-    "apiKey": "6d095bcd2c8af4cfa16b9a2c446909f1"
-    fethcWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey "6d095bcd2c8af4cfa16b9a2c446909f1")
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-        var { city } = data;
-        var { icon, description } = data.weather[0];
-        var { temp, humidity } = data.main;
-        var { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed)
-        document.querySelector(".city").innerText = "Weather in" + name;
-        document.querySelector(".icon").src = "https://developer.accuweather.com/sites/default/files/07-s.png"
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "m/hr";
-        document.querySelector(".uvIndex").innerText = "UV Index: " + uvIndex;
-    }
-    search: funtion () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    }
-};
+let searchBtn = document.querySelector('#searchBtn');
+let searchInput = document.querySelector('.search-bar');
+let cityNameEl = document.querySelector('#city-name');
+let cityTempEl = document.querySelector('.temp');
+let descriptionEl = document.querySelector('.description');
+let humidityEl = document.querySelector('#humid');
+let windEl = document.querySelector('#wind-speed');
+let uvIndexEl = document.querySelector('#uv-index');
+let weatherImgEl = document.querySelector('#weather-icon');
 
-localStorage.setItem("city", "description", "temp", "humidity", "speed" );
+function printCityWeather(weather) {
+  cityNameEl.textContent = weather.name;
+  cityTempEl.textContent = weather.main.temp + ' °F';
+  descriptionEl.textContent = weather.weather[0].description;
+  humidityEl.textContent = weather.main.humidity;
+  windEl.textContent = weather.wind.speed;
+  weatherImgEl.src =
+    'https://openweathermap.org/img/w/' + weather.weather[0].icon + '.png';
+}
 
-document.querySelector(".search button").addEventListener("click", function () {
-    document.querySelector(".search-bar").addEventListener("")
-    if ()
-        weather.search();
-});
+function cityCurrentWeather(city) {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      city +
+      '&units=imperial&appid=' +
+      '6d095bcd2c8af4cfa16b9a2c446909f1',
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      printCityWeather(data);
+    });
+}
+
+function saveCityToLocalStorage(city) {
+  let cityValues = [];
+
+  if (localStorage.getItem('city-names')) {
+    cityValues = JSON.parse(localStorage.getItem('city-names'));
+  }
+
+  cityValues.push(city);
+  localStorage.setItem('city-names', JSON.stringify(cityValues));
+}
+
+function searchCityWeather() {
+  let city = searchInput.value;
+  cityCurrentWeather(city);
+  saveCityToLocalStorage(city);
+}
+
+searchBtn.addEventListener('click', searchCityWeather);
